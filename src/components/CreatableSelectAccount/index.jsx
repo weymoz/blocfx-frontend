b59f0441573430976d922660c2cbc022bbd79c6c
weymoz <weymoze@gmail.com>
2019-12-20 15:41:55 +0200

    adds new accounts

--- src/components/CreatableSelectAccount/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import s from './style.scss'
-
-export default function CreatableSelectAccount() {
-    return (
-        <h1>Creatable Select</h1>
-    )
-}
--- src/data/accounts.js
+++ src/data/accounts.js
@@ -7,11 +7,56 @@ export default [
     {
         bank: "UniCredit",
         number: "2344 8776 2344 9987",
-        currency: "USD"
+        currency: "GBP"
     },
     {
         bank: "Deutche Bank",
         number: "1249 8754 2306 9862",
+        currency: "EUR"
+    },
+    {
+        bank: "Bank of China",
+        number: "1249 8754 2306 9862",
+        currency: "CNY"
+    },
+    {
+        bank: "JPMorgan Chase & Co.",
+        number: "5168 2323 41234 4219",
         currency: "USD"
+    },
+    {
+        bank: "HSBC Holdings PLC",
+        number: "2344 8776 2344 9987",
+        currency: "GBP"
+    },
+    {
+        bank: "Banco Santander",
+        number: "1249 8754 2306 9862",
+        currency: "EUR"
+    },
+    {
+        bank: "Mizuho Financial Group",
+        number: "1249 8754 2306 9862",
+        currency: "CNY"
+    },
+    {
+        bank: "Bank of America	",
+        number: "5168 2323 41234 4219",
+        currency: "USD"
+    },
+    {
+        bank: "Barclays PLC",
+        number: "2344 8776 2344 9987",
+        currency: "GBP"
+    },
+    {
+        bank: "Credit Suisse",
+        number: "1249 8754 2306 9862",
+        currency: "EUR"
+    },
+    {
+        bank: "KB Financial Group Inc",
+        number: "1249 8754 2306 9862",
+        currency: "CNY"
     },
 ]
